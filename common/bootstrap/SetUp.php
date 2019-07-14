efbd2535f8fd26c71e8d1eb7865864773c74a414
Расуль Арсланов <rasularslanoov@gmail.com>
2019-07-14 12:27:13 +0500

    Added Bootstrap class

---
+++ common/bootstrap/SetUp.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace common\bootstrap;
+
+use yii\base\BootstrapInterface;
+use Yii;
+
+class SetUp implements BootstrapInterface
+{
+    public function bootstrap($app)
+    {
+        $container = Yii::$container;
+
+
+    }
+}
