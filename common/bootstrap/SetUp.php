aaeac0fed90ec0abf566948dcdc1c560a35ab265
Расуль Арсланов <rasularslanoov@gmail.com>
2019-07-19 22:35:44 +0500

    Fixed bug with EventDispatcher

--- common/bootstrap/SetUp.php
+++ common/bootstrap/SetUp.php
@@ -3,37 +3,37 @@
 namespace common\bootstrap;
 
 use store\dispatchers\DeferredEventDispatcher;
-use store\dispatchers\SimpleEventDispatcher;
-use store\entities\User\events\UserSignUpConfirmed;
-use store\entities\User\events\UserSignUpRequested;
-use store\listeners\User\UserSignUpConfirmedListener;
-use store\listeners\User\UserSignupRequestedListener;
+use store\dispatchers\EventDispatcher;
+use store\listeners\User\UserSignUpRequestedListener;
 use store\useCases\ContactService;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use yii\base\BootstrapInterface;
+use store\dispatchers\SimpleEventDispatcher;
+use store\listeners\User\UserSignUpConfirmedListener;
 use Yii;
+use yii\base\BootstrapInterface;
 use yii\di\Container;
 use yii\mail\MailerInterface;
 use yii\rbac\ManagerInterface;
+use store\entities\User\events\UserSignUpRequested;
+use store\entities\User\events\UserSignUpConfirmed;
 
 class SetUp implements BootstrapInterface
 {
     public function bootstrap($app)
     {
         $container = Yii::$container;
-
+        
         $container->setSingleton(MailerInterface::class, function () use ($app) {
             return $app->mailer;
         });
-
+        
         $container->setSingleton(ManagerInterface::class, function () use ($app) {
             return $app->authManager;
         });
-
+        
         $container->setSingleton(ContactService::class, [], [
             $app->params['adminEmail']
         ]);
-
+        
         $container->setSingleton(EventDispatcher::class, DeferredEventDispatcher::class);
         $container->setSingleton(DeferredEventDispatcher::class, function (Container $container) {
             return new DeferredEventDispatcher(new SimpleEventDispatcher($container, [
@@ -41,6 +41,7 @@ class SetUp implements BootstrapInterface
                 UserSignUpConfirmed::class => [UserSignupConfirmedListener::class],
             ]));
         });
+        
         return new SimpleEventDispatcher($container, [
             UserSignUpRequested::class => [UserSignupRequestedListener::class],
             UserSignUpConfirmed::class => [UserSignupConfirmedListener::class],
